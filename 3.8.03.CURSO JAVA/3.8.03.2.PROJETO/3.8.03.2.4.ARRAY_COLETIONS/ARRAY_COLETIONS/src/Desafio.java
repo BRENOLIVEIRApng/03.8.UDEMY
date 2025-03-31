bda58cd8c6683b4c6eddb51dd20bd9c4a1a59094
BRENOLIVEIRApng <brenoreisrv@gmail.com>
2025-03-31 09:10:42 -0300

    Create Desafio.java

---
+++ 3.8.03.CURSO JAVA/3.8.03.2.PROJETO/3.8.03.2.4.ARRAY_COLETIONS/ARRAY_COLETIONS/src/Desafio.java
@@ -0,0 +1,10 @@
+public class Desafio {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Quantas notas");
+        
+        entrada.close();
+    }
+
+}
