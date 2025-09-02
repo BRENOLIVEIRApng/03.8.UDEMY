b3e11f45478a715293df58aa9b8e590bb6e04aa5
BRENOLIVEIRApng <brenoreisrv@gmail.com>
2025-09-02 11:41:34 -0300

    Update carro.java

--- 3.8.03.CURSO JAVA/3.8.03.2.PROJETO/3.8.03.2.5.ORIENTACAO_OBJETOS/ORIENTACAO/src/oo/composicao/carro.java
+++ 3.8.03.CURSO JAVA/3.8.03.2.PROJETO/3.8.03.2.5.ORIENTACAO_OBJETOS/ORIENTACAO/src/oo/composicao/carro.java
@@ -4,10 +4,26 @@ public class carro {
     Motor motor = new Motor();
 
     void acelerar (){
+        if (motor.fatorInjecao < 2.6){
         motor.fatorInjecao += 0.4;
+        }
     }
 
     void frear(){
+        if(motor.fatorInjecao > 0.5){
         motor.fatorInjecao -= 0.4;
+        }
+    }
+
+    void ligar (){
+        motor.ligado = true;
+    }
+
+    void desligar (){
+        motor.ligado = false;
+    }
+
+    void estaLigado(){
+        return motor.ligado;
     }
 }
