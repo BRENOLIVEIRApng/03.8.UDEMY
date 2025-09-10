5bd82cb7cb77d57b31514aff42e6898545acb6e4
BRENOLIVEIRApng <brenoreisrv@gmail.com>
2025-09-09 21:36:49 -0300

    renderizacao

--- 3.8.03.CURSO JAVA/3.8.03.2.PROJETO/3.8.03.2.6.GERENCIADOR_TAREFAS/gerenciador_tarefas/src/main/java/com/breno/gerenciador_tarefas/controller/PaginaController.java
+++ 3.8.03.CURSO JAVA/3.8.03.2.PROJETO/3.8.03.2.6.GERENCIADOR_TAREFAS/gerenciador_tarefas/src/main/java/com/breno/gerenciador_tarefas/controller/PaginaController.java
@@ -4,24 +4,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @Controller
 public class PaginaController {
 
     @GetMapping("/")
-    public String home(Model model) {
+    public String home(Model model, HttpServletRequest request) {
         model.addAttribute("titulo", "Início");
         model.addAttribute("cssPagina", "home");
         model.addAttribute("jsPagina", "tarefas");
         model.addAttribute("conteudo", "tarefas");
+        model.addAttribute("requestURI", request.getRequestURI());
         return "base";
     }
 
     @GetMapping("/tarefas")
-    public String tarefas(Model model) {
+    public String tarefas(Model model, HttpServletRequest request) {
         model.addAttribute("titulo", "Lista de Tarefas");
         model.addAttribute("cssPagina", "tarefas");
         model.addAttribute("jsPagina", "tarefas");
         model.addAttribute("conteudo", "tarefas");
+        model.addAttribute("requestURI", request.getRequestURI());
         return "base";
     }
 }
